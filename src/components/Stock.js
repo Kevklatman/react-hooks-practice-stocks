9f67daa85b83ac43f157ce02345df0617607201e
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-13 23:38:31 -0600

    hjgfcg

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,12 +1,15 @@
 import React from "react";
+import stocks from "../db.json";
+
 
 function Stock() {
-  return (
+  stocks.map((stock)=> 
+   
     <div>
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+          <h5 className="card-title">{stock.name}</h5>
+          <p className="card-text">{stock.price}</p>
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer(){
   return (
+  
     <div>
       <h2>Stocks</h2>
+      <li>
+      <Stock/>
+      </li>
       {/* render stock list here*/}
     </div>
   );
